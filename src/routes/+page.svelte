ac4e08354245ec4d5f870921ba33c5babfe3e0af
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2024-03-28 20:34:34 +0200

    ux upgrade

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import init, { convert, alter_identifier } from "$lib/wasm/embolden.js";
     import { mergeUint8Arr, incompressibleExt } from "$lib/_helpers/index";
-    import { Info } from "lucide-svelte";
+    import { Info, X } from "lucide-svelte";
 
     import { Input, type FormInputEvent } from "$lib/components/ui/input";
     import { Label } from "$lib/components/ui/label";
@@ -13,9 +13,13 @@
     import { Separator } from "$lib/components/ui/separator";
 
     let urls: { filename: string; url: string }[] = [];
+    // urls.push({ filename: "1", url: "aotenarsotea" });
+    // urls.push({ filename: "2", url: "aotenarsotea" });
+    // urls.push({ filename: "3", url: "aotenarsotea" });
+    // urls.push({ filename: "4", url: "aotenarsotea" });
     let overwrite = false;
     let bold_fullstop = false;
-    // $: console.log(urls);
+    $: console.log(urls);
 
     const convertFile = async (event: FormInputEvent<Event>) => {
         let target = event.target as HTMLInputElement;
@@ -84,10 +88,8 @@
     };
 </script>
 
-<div
-    class="container py-10 max-w-[500px] overflow-hidden flex-center flex-col min-h-svh gap-5"
->
-    <div class="flex flex-col gap-5">
+<div class="container mx-auto justify-center items-center overflow-hidden grid grid-cols-1 md:grid-cols-2 min-h-svh gap-5">
+    <div class="flex justify-end md:justify-normal flex-col gap-5 px-10 min-h-[200px]">
         <div class="">
             <Label class="w-full">Upload a book to begin</Label>
             <Input
@@ -125,65 +127,53 @@
                 </Tooltip.Root>
             </Label>
         </div>
-        <!-- <div class="flex items-center space-x-2"> -->
-        <!--     <Switch bind:checked={overwrite} /> -->
-        <!--     <Label> -->
-        <!--         Overwrite Existing -->
-        <!--         <Tooltip.Root> -->
-        <!--             <Tooltip.Trigger> -->
-        <!--                 <Info -->
-        <!--                     class="hover:text-primary/60 inline h-4 w-4 align-top" -->
-        <!--                 /> -->
-        <!--             </Tooltip.Trigger> -->
-        <!--             <Tooltip.Content> -->
-        <!--                 <p class="max-w-[200px]"> -->
-        <!--                     When enabled, this will change the identifier of the -->
-        <!--                     book, so that there will be two "copies" of the -->
-        <!--                     book inside the ebook reader.  -->
-        <!--                 </p> -->
-        <!--             </Tooltip.Content> -->
-        <!--         </Tooltip.Root> -->
-        <!--     </Label> -->
-        <!-- </div> -->
     </div>
-    <Separator />
+    <!-- <Separator orientation='vertical'/> -->
     <div
         class:hidden={urls.length === 0}
-        class="justify-end gap-5 flex w-full flex-col"
+        class="gap-5 flex min-h-svh  px-10 justify-start flex-col"
     >
-        {#each urls as url}
-            <div
-                class="w-full py-2 px-2"
-                bind:innerText={url.filename}
-                contenteditable="plaintext-only"
-                aria-readonly={false}
-                role="textbox"
-                tabindex="0"
-            >
-                {url.filename}
-            </div>
-            <div class="flex justify-end items-center gap-5">
-                <Popover.Root>
-                    <Popover.Trigger
-                        ><Button variant="ghost">Rename</Button
-                        ></Popover.Trigger
-                    >
-                    <Popover.Content
-                        class="flex flex-col justify-end items-end gap-3"
-                    >
-                        <Input
-                            type="text"
-                            placeholder="New Name"
-                            bind:value={url.filename}
-                        />
-                        <Popover.Close>
-                            <Button variant="ghost">Done</Button>
-                        </Popover.Close>
-                    </Popover.Content>
-                </Popover.Root>
-                <a href={url.url} download={`EMBOLDEN_${url.filename}`}>
-                    <Button>Download</Button>
-                </a>
+        {#each urls as url, index}
+            <div class="rounded-md py-4 px-6 relative">
+                <Button
+                    variant="ghost"
+                    on:click={() => (urls = urls.filter((_, i) => index != i))}
+                    class="absolute hover:bg-transparent hover:opacity-50 -top-2 -right-2"
+                >
+                    <X class="h-4 w-4 " />
+                </Button>
+                <div
+                    class="w-full py-2 px-4 hover:cursor-pointer"
+                    bind:innerText={url.filename}
+                    contenteditable="plaintext-only"
+                    aria-readonly={false}
+                    role="textbox"
+                    tabindex="0"
+                >
+                    {url.filename}
+                </div>
+                <div class="flex justify-end items-center gap-5 pt-2">
+                    <Popover.Root>
+                        <Popover.Trigger>
+                            <Button variant="ghost">Rename</Button>
+                        </Popover.Trigger>
+                        <Popover.Content
+                            class="flex flex-col justify-end items-end gap-3"
+                        >
+                            <Input
+                                type="text"
+                                placeholder="New Name"
+                                bind:value={url.filename}
+                            />
+                            <Popover.Close>
+                                <Button variant="ghost">Done</Button>
+                            </Popover.Close>
+                        </Popover.Content>
+                    </Popover.Root>
+                    <a href={url.url} download={`EMBOLDEN_${url.filename}`}>
+                        <Button>Download</Button>
+                    </a>
+                </div>
             </div>
         {/each}
     </div>
