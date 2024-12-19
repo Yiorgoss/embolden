d9ab7c5e87e213854f7f0b1ca9a6220b67c78a70
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2024-12-19 20:42:32 +0200

    removed css parsing, now handled with js. plus bugfixes

--- converter/src/lib.rs
+++ converter/src/lib.rs
@@ -1,24 +1,41 @@
 mod add_global_styles;
 mod alter_identifier;
 mod convert_to_bold;
+mod custom_font;
 
 use wasm_bindgen::prelude::*;
+//
+// #[wasm_bindgen]
+// pub fn add_css(
+//     arr: &[u8],
+//     no_custom_font: bool,
+//     undo_text_transform: bool,
+//     font_weight: usize,
+//     bold_weight: usize,
+//     font_path: &str,
+//     font_name: &str,
+// ) -> Vec<u8> {
+//     //quick fix cos im lazy but refactor to using just javaasrcipt will be necessaary since this is
+//     //a fuckign xml parser and im writing css smh
+//     add_global_styles::add_css(
+//         arr,
+//         no_custom_font,
+//         undo_text_transform,
+//         font_weight,
+//         bold_weight,
+//         font_path,
+//         font_name,
+//     )
+// }
+
+// #[wasm_bindgen]
+// pub fn insert_custom_font(input_file: &[u8], string: &str) -> Vec<u8> {
+//     custom_font::insert_custom_font(input_file, string)
+// }
 
 #[wasm_bindgen]
-pub fn add_css(
-    arr: &[u8],
-    no_custom_font: bool,
-    undo_text_transform: bool,
-    font_weight: usize,
-    bold_weight: usize,
-) -> Vec<u8> {
-    add_global_styles::add_css(
-        arr,
-        no_custom_font,
-        undo_text_transform,
-        font_weight,
-        bold_weight,
-    )
+pub fn insert_into_content_opf(input_file: &[u8], font_name: &str) -> Vec<u8> {
+    custom_font::insert_into_content_opf(input_file, font_name)
 }
 
 #[wasm_bindgen]
