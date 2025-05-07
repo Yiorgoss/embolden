df06130a6ff9e5626d50ae56f56f8852e9f2a908
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2025-05-07 15:09:02 +0300

    added ssg

--- src/routes/[...slug]/+page.server.ts
+++ src/routes/[...slug]/+page.server.ts
@@ -1,20 +1,20 @@
+import type { Page } from '@payload-types';
 import type { EntryGenerator } from './$types';
 import { site } from '@/config';
 
-// export const entries: EntryGenerator = async () => {
-//   const response = fetch(`${site.CMS}/api/tenants?[where][domainName][equal]=${site.domainName}`)
-//     .then((res) => res.json())
-//     .then((json) => ({
-//       //@ts-ignore
-//       nav: json.docs[0].nav,
-//       //@ts-ignore
-//       pages: json.docs[0].pages
-//     }));
-//
-//   const { pages } = await response
-//   const slugList = pages.map(({ slug: string }) => ({ slug: slug }));
-//
-//   return slugList;
-// };
-//
-// export const prerender = true
+
+export const entries: EntryGenerator = async () => {
+  const response = fetch(`${site.CMS}/api/tenants?[where][domainName][equal]=${site.domainName}`)
+    .then((res) => res.json())
+    .then((json) => ({
+      //@ts-ignore
+      pages: json.docs[0].pages
+    }));
+
+  const { pages } = await response;
+  const slugList = pages.docs.map(({ slug }: { slug: string }) => ({ slug: slug.charAt(0) == '/' ? slug.slice(1) : slug }));
+
+  return slugList;
+};
+
+export const prerender = true;
