001b94e3aa08dc225f54093956a97aafbd863baa
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2025-06-13 16:43:26 +0300

    minor css

--- src/lib/components/blocks/hero/calisto-landing.svelte
+++ src/lib/components/blocks/hero/calisto-landing.svelte
@@ -28,24 +28,18 @@
 			/>
 		{/if}
 	</div>
-	<div
-		class="z-10 col-span-full row-span-full h-full w-full bg-background/20 mix-blend-lighten"
-	></div>
 	<div class="z-10 col-span-full row-start-2 row-end-4 container px-5">
-		{#key bgLoaded}
-			<div
-				in:fade={{ delay: 500, duration: 1000 }}
-				class:hidden={!bgLoaded}
-				class="flex h-full max-w-fit flex-col items-start justify-center"
-			>
-				{#if richText}
-					<RichTextRender {richText} />
-				{/if}
-				<div class="flex gap-5 self-start pt-10 md:self-end">
-					<Button class="px-5  py-5" link={ctaTwo} />
-					<Button class="px-5  py-5" link={ctaOne} />
-				</div>
+		<div
+			class:opacity-100={bgLoaded}
+			class="flex opacity-0 transition-opacity delay-500 duration-1000 h-full max-w-fit flex-col items-start justify-center"
+		>
+			{#if richText}
+				<RichTextRender {richText} />
+			{/if}
+			<div class="flex gap-5 self-start pt-10 md:self-end">
+				<Button class="px-5  py-5" link={ctaTwo} />
+				<Button class="px-5  py-5" link={ctaOne} />
 			</div>
-		{/key}
+		</div>
 	</div>
 </div>
