fa117536cb1c4ca5c09176420eb47287078a584b
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2025-09-10 23:04:09 +0300

    minor dev

--- src/routes/[[locale=locale]]/[...slug]/+layout.server.ts
+++ src/routes/[[locale=locale]]/[...slug]/+layout.server.ts
@@ -1,3 +1,4 @@
+import { PUBLIC_ENV } from '$env/static/public';
 import type { LayoutServerLoad } from './$types';
 import { site, defaultLocale } from '@/config';
 import { fetchFromCMS, getTenantByDomain } from '@/utils';
@@ -7,6 +8,9 @@ import { error } from '@sveltejs/kit';
 
 
 export const load: LayoutServerLoad = async (args) => {
+  if (PUBLIC_ENV != "PROD") {
+    console.log({ site })
+  }
   const { platform, params } = args
 
   const locale = params.locale ?? defaultLocale
