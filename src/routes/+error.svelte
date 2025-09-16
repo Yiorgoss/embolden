a4a31afd36bf91a769558759f45d11b83418ffb8
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2025-09-16 18:34:18 +0300

    new error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,7 @@
-<div class="flex h-svh w-full items-center justify-center">hi</div>
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<h1 class="flex h-svh w-full items-center justify-center">
+	{page?.error?.message ?? 'Error Loading Page'}
+</h1>
