da4474fe5c1a4ad53faa3005515e76a0d1cdc336
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2025-09-22 00:00:38 +0300

    on-demand importing of blocks, not quite there yet with icons

--- src/lib/components/blocks/render-blocks.svelte
+++ src/lib/components/blocks/render-blocks.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { blockList } from '$lib/components/blocks';
+	import { onMount, type ComponentType } from 'svelte';
 
 	const {
 		blockData,
@@ -8,10 +9,60 @@
 		hasLocaleSwitch
 	}: { blockData: any; className?: string; cb?: () => void; hasLocaleSwitch?: boolean } = $props();
 
-	const calculatedBlockType = blockList.get(blockData?.blockType ?? undefined) ?? undefined;
-	const SvelteComponent = $derived(calculatedBlockType);
+	const dynamicResolveBlock = async ({ blockName }: { blockName: string }) => {
+		switch (blockName) {
+			case 'calistoLanding':
+				return import('./hero/calisto-landing.svelte');
+			case 'mediumHero':
+				return import('./hero/medium-hero.svelte');
+			case 'stickyContainers':
+				return import('./special/sticky-containers.svelte');
+			case 'bentoGrid':
+				return import('./special/bento-grid.svelte');
+			case 'accordion':
+				return import('./special/accordion.svelte');
+			case 'carousel':
+				return import('./special/carousel.svelte');
+			case 'calistoFeatureCard':
+				return import('./cards/calisto-feature-card.svelte');
+			case 'discountCard':
+				return import('./cards/discount-card.svelte');
+			case 'singleCard':
+				return import('./cards/single-card.svelte');
+			case 'hoverCard':
+				return import('./cards/hover-card.svelte');
+			case 'blockColumnLayout':
+				return import('./layout/block-column-layout.svelte');
+			case 'contentColLayout':
+				return import('./layout/content-col-layout.svelte');
+			case 'image':
+				return import('./common/image.svelte');
+			case 'gutter':
+				return import('./common/gutter.svelte');
+			case 'rtBlock':
+				return import('./rich-text/block.svelte');
+			case 'imageHeader':
+				return import('./navigation/header/image-header.svelte');
+			case 'richTextFooter':
+				return import('./navigation/footer/rich-text-footer.svelte');
+			case 'contactForm':
+				return import('./forms/contact-form.svelte');
+		}
+	};
+
+	let calculatedBlockType = $state();
+	onMount(async () => {
+		const blockName = blockData?.blockType ?? undefined;
+		calculatedBlockType = await dynamicResolveBlock({ blockName });
+	});
+
+	//  const calculatedBlockType = blockList.get(blockData?.blockType ?? undefined) ?? undefined;
+	//  const SvelteComponent = $derived(calculatedBlockType.default);
 </script>
 
 {#if calculatedBlockType}
-	<SvelteComponent {className} {blockData} {cb} {hasLocaleSwitch} />
+	{#await calculatedBlockType then B}
+		{@const Block = B.default}
+		<Block {className} {blockData} {cb} {hasLocaleSwitch} />
+	{/await}
 {/if}
--- src/lib/components/common/icon.svelte
+++ src/lib/components/common/icon.svelte
@@ -41,3 +41,63 @@
 {#if calculatedBlockType}
 	<IconComponent class={className} />
 {/if}
+<!--  <script lang="ts">
+	//  import {
+	//  	CircleArrowRight,
+	//  	ArrowRight,
+	//  	ArrowLeft,
+	//  	LoaderCircle,
+	//  	Users,
+	//  	Cog,
+	//  	Accessibility,
+	//  	MonitorSmartphone,
+	//  	ChevronDown,
+	//  	Menu
+	//  } from '@lucide/svelte';
+	import { onMount } from 'svelte';
+	import { type IIcon } from '@payload-types';
+
+	const {
+		icon,
+		name,
+		class: className
+	}: { icon?: IIcon; name?: string; class?: string } = $props();
+
+	const dynamicResolveBlock = async ({ iconName }: { iconName: string }) => {
+		switch (iconName) {
+			case 'users':
+				return import('@lucide/svelte/icons/users');
+			case 'menu':
+				return import('@lucide/svelte/icons/menu');
+			case 'cog':
+				return import('@lucide/svelte/icons/cog');
+			case 'accessibility':
+				return import('@lucide/svelte/icons/accessibility');
+			case 'responsive':
+				return import('@lucide/svelte/icons/monitor-smartphone');
+			case 'chevron-down':
+				return import('@lucide/svelte/icons/chevron-down');
+			case 'circle-arrow-right':
+				return import('@lucide/svelte/icons/circle-arrow-right');
+			case 'arrow-right':
+				return import('@lucide/svelte/icons/arrow-right');
+			case 'arrow-left':
+				return import('@lucide/svelte/icons/arrow-left');
+			case 'loader-circle':
+				return import('@lucide/svelte/icons/loader-circle');
+		}
+	};
+
+	let iconPromise = $state();
+	onMount(async () => {
+		const iconName = icon?.name ?? name ?? undefined;
+		iconPromise = await dynamicResolveBlock(iconName);
+	});
+</script>
+
+{#if iconPromise}
+	{#await iconPromise then I}
+		{@const Icon = I.default}
+		<Icon {className} {blockData} {cb} {hasLocaleSwitch} />
+	{/await}
+{/if}  -->
