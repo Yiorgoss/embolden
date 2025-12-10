2a4e1f04fcf60a315de2cfcce0732b1fe45f8113
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2025-12-10 13:16:00 +0200

    changed icons to web component

--- src/lib/components/common/icon.svelte
+++ src/lib/components/common/icon.svelte
@@ -1,22 +1,12 @@
 <script lang="ts">
-	import { cn } from '@/utils';
 	import type { IIcon } from '@payload-types';
 	import 'iconify-icon';
 
 	const {
 		icon,
 		name,
-		class: className,
-		style
-	}: { icon?: IIcon; name?: string; class?: string; style?: string } = $props();
+		class: className
+	}: { icon?: IIcon; name?: string; class?: string } = $props();
 </script>
 
-<iconify-icon
-	height="none"
-	width="none"
-	icon={name ?? icon?.name ?? ''}
-	class={className}
-	style:height="inherit"
-	style:width="inherit"
-	style={style ?? icon?.style}
-></iconify-icon>
+<iconify-icon icon={name ?? icon?.name ?? ''} class={className} style={icon?.style}></iconify-icon>
