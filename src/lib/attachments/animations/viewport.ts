d86fe322e1c4a51e1d16e796aee2f2464f8faca6
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2025-12-11 23:58:51 +0200

    added a viewport attachment

---
+++ src/lib/attachments/animations/viewport.ts
@@ -0,0 +1,58 @@
+import { animate, inView as inViewMotion, stagger, type AnimationPlaybackControlsWithThen, type MotionNodeOptions } from "motion";
+import type { Attachment } from "svelte/attachments";
+
+const animationList = {
+  'slideUpFadeIn': {
+    "entry": {
+      initialCSS: "transform: translateY(100px);", // not the prettiest
+      transforms: { opacity: [0, 1], y: [0, -100] },
+      options: { duration: 0.3, ease: 'easeOut', delay: stagger(0.1) }
+
+    },
+    "exit": {
+      transforms: { opacity: [1, 0], y: [-100, 0] },
+      options: { duration: 0.3, ease: 'easeOut', delay: stagger(0.1) }
+    }
+  }
+}
+
+type animationListKeys = keyof typeof animationList;
+
+export function animateViewport(
+  preset: animationListKeys,
+  { child = "#animate-child", amount = 0.5 }:
+    { child?: string, amount?: number } = {})
+  : Attachment {
+
+  // FIX - animation options doesnt have the right types and I CBF to deal with this shit right now right now
+  //     - find a better way to deal with intial state of css for animations
+  //         - this is because the cleanup function has not run yet, so state = 0 
+
+  const { entry, exit } = animationList[preset]
+  return (element) => {
+    element.setAttribute("style", entry.initialCSS)
+    const cleanup = inViewMotion(
+      element,
+      (el) => {
+        const cards = el.querySelectorAll(child);
+        const inAnimation = animate(
+          cards,
+          entry.transforms,
+          //@ts-ignore 
+          entry.options
+        );
+        return () => {
+          inAnimation.cancel()
+          animate(
+            cards,
+            exit.transforms,
+            //@ts-ignore
+            exit.options
+          );
+        }
+      },
+      { amount: amount }
+    );
+    return () => cleanup();
+  }
+}
