3a0a94553e871d45e76d3673c2454f6d4ebc1972
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2026-01-29 19:03:15 +0200

    rremoved caached git files

--- transfer/old/app/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import EpubConverter from "$lib/components/app/epub-converter.svelte";
-</script>
-
-<div class="pt-[100px]">
-    <EpubConverter />
-</div>
--- transfer/old_src/lib/functions/types.ts
+++
@@ -1,3 +0,0 @@
-export type Font =
-  | { name: string; ext: string; u8Data: Uint8Array }
-  | undefined;
