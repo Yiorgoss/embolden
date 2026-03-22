ed34a1cbb61380b9901f0745920685a3fa8a2742
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2026-03-22 20:54:26 +0200

    added image background to richtextcard

--- src/lib/components/blocks/cards/rich-text-card.svelte
+++ src/lib/components/blocks/cards/rich-text-card.svelte
@@ -2,15 +2,17 @@
 	import type { IRichTextCard } from '@payload-types';
 	import { RichTextRender } from '../rich-text';
 	import * as Card from '@/components/ui/card';
+	import Picture from '@/components/common/picture.svelte';
 
 	const { blockData }: { blockData: IRichTextCard } = $props();
-	const { richText, style: { background, alignX, alignY } = {} } = $derived(blockData);
+	const { richText, style: { background, alignX, alignY, borderRadius, maxWidth, border } = {} } =
+		$derived(blockData);
 </script>
 
 <section id="rich-text-card-block" class="flex items-center justify-center h-full w-full py-8">
 	<Card.Root
-		style={`background:${background};`}
-		class="h-full max-w-xs w-full hover:scale-105 transition-transform duration-300"
+		style={`background:${background};border:${border};border-radius:${borderRadius};max-width:${maxWidth};`}
+		class="relative overflow-hidden h-full col-start-1 row-start-1 max-w-xs w-full hover:scale-105 transition-transform duration-300"
 	>
 		<Card.Content
 			style={`justify-content:${alignX};align-items:${alignY}`}
@@ -18,5 +20,10 @@
 		>
 			<RichTextRender {richText} />
 		</Card.Content>
+		{#if blockData?.image}
+			<div class="absolute inset-0">
+				<Picture image={blockData?.image} />
+			</div>
+		{/if}
 	</Card.Root>
 </section>
