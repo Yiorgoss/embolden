e8cc8c7f666ac6a71c7608e4ba32684b446c96fc
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2026-04-01 00:16:15 +0300

    time to test worker

--- scripts/add_domain_wrangler.sh
+++ scripts/add_domain_wrangler.sh
@@ -3,8 +3,8 @@
 
 source .env
 
-if [[ -z $IS_BASE ]]; then
-  echo "xx"
+if [[ -n "$IS_BASE" ]]; then
+  echo "Changing Domain Name ..."
   sed -i '' -Ee "s/%domainName%/$PUBLIC_DOMAIN_NAME/g" wrangler.jsonc
 else
   echo "You are trying to push the base template...Are you sure you want to do this?"
